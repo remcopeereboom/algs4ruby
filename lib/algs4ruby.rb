876bcbe611ae0232cab9fce9e5e57d8b1441d1a2
Remco Peereboom <rpeere@yandex.com>
2016-03-17 15:50:20 +0100

    Implemented Bag (Bag)
    Added all methods from the Java library. Included the Enumerable
    library to make the class slightly more useable from a ruby
    perspective.
    
    Added tests and documentation for the methods.
    
    Implementeed the testing client to have the same behaviour as the
    original Java library.
    
    Added license information.

--- lib/algs4ruby.rb
+++ lib/algs4ruby.rb
@@ -1,4 +1,6 @@
 require 'algs4ruby/version'
 
+require 'algs4ruby/fundamentals/bag'
+
 require 'algs4ruby/standard_io_libraries/standard_random'
 require 'algs4ruby/standard_io_libraries/standard_stats'
