bcdfb8c446506ac134f248415d2f5c3d5398bf09
Remco Peereboom <rpeere@yandex.com>
2016-03-17 16:59:27 +0100

    Implemented StackLinkedList (LinkedStack)
    Added all the methods from the Java library. Included the Enumerable
    library to make the class slightly more useable from a ruby perspective.
    
    Added tests fort all the methods.
    
    Added documentation for all the methods.
    
    Implemented the testing client to have the same behaviour as the
    original library.
    
    Added license information.

--- lib/algs4ruby.rb
+++ lib/algs4ruby.rb
@@ -7,6 +7,7 @@ require 'algs4ruby/fundamentals/bag_linked_list'
 require 'algs4ruby/fundamentals/bag_resizing_array'
 
 require 'algs4ruby/fundamentals/stack'
+require 'algs4ruby/fundamentals/stack_linked_list'
 
 require 'algs4ruby/standard_io_libraries/standard_random'
 require 'algs4ruby/standard_io_libraries/standard_stats'
