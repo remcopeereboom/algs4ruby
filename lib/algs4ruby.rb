a4590f3cef8da2107dec4453f5b5c81d32157e52
Remco Peereboom <rpeere@yandex.com>
2016-03-17 16:26:11 +0100

    Implemented BagResizingArray (ResizingArrayBag)
    Added all the methods from the Java library. Included the Enumerable
    library to make the class slightly more useable from a ruby perspective.
    
    Added tests for the methods.
    
    Added documentation for the methods.
    
    Implemented the testing client to have the same behaviour as the
    original Java library.
    
    Added license information.

--- lib/algs4ruby.rb
+++ lib/algs4ruby.rb
@@ -2,6 +2,7 @@ require 'algs4ruby/version'
 
 require 'algs4ruby/fundamentals/bag'
 require 'algs4ruby/fundamentals/bag_linked_list'
+require 'algs4ruby/fundamentals/bag_resizing_array'
 
 require 'algs4ruby/standard_io_libraries/standard_random'
 require 'algs4ruby/standard_io_libraries/standard_stats'
---
+++ spec/algs4ruby/fundamentals/bag_resizing_array_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+module Algs4Ruby
+  describe BagResizingArray do
+    it_behaves_like 'a bag'
+  end
+end
