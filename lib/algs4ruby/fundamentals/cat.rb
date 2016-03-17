4f82c188f273661d38880ce6e29df56c274262a8
Remco Peereboom <rpeere@yandex.com>
2016-03-17 19:13:29 +0100

    Implemented Cat (Cat)

--- lib/algs4ruby/fundamentals/cat.rb
+++ lib/algs4ruby/fundamentals/cat.rb
@@ -1,24 +1,18 @@
-module Algorithms
+module Algs4Ruby
   # Testing client
-  #   ruby average.rb < file1 file2 file3
-  #   ruby average.rb 23.4 123 12.4
+  #   ruby average.rb in1 in2 in3 out
   #
-  # Read lines from ARGF and converts each line to a floating point number.
-  # Computes the average of those numbers and prints it to standard out.
+  # Takes a list of n files from the command-line and concatenates the
+  # first n - 1 and writes them to the nth file.
   if __FILE__ == $PROGRAM_NAME
-    count = 0
-    sum = 0.0
-    ARGF.readlines.each do |l|
-      sum += l.to_f
-      count += 1
+    File.open(ARGV.pop, 'w') do |f|
+      ARGF.readlines.each { |l| f.write(l) }
     end
-
-    puts "Average is #{sum / count}."
   end
 end
 
 ###############################################################################
-# The code in this file is based on the java code in Average from alg4.jar
+# The code in this file is based on the java code in Cat from alg4.jar
 # library. That library can be found at http://algs4.cs.princeton.edu
 # You can find more information on the alg4.jar library on that website.
 #
@@ -27,15 +21,15 @@ end
 # Kevin Wayne. Attribution of the algorithms can be found at the princeton
 # website as well.
 #
-# average.rb is free software: you can redistribute it and/or modify
+# cat.rb is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# average.rb is distributed in the hope that it will be useful,
+# cat.rb is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 # You should have received a copy of the GNU General Public License
-# along with average.rb. If not, see http://www.gnu.org/licenses.
+# along with cat.rb. If not, see http://www.gnu.org/licenses.
 ################################################################################
