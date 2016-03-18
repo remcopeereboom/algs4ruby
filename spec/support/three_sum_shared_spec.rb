ce188f2b88b6c9398e127035905022f93024b3d9
Remco Peereboom <rpeere@yandex.com>
2016-03-18 16:01:11 +0100

    Moved three sum specs to a shared spec file.

---
+++ spec/support/three_sum_shared_spec.rb
@@ -0,0 +1,52 @@
+module Algs4Ruby
+  RSpec.shared_examples 'three sum' do
+    describe '.count' do
+      context 'given an empty array' do
+        let(:a) { [] }
+
+        it 'returns 0' do
+          expect(described_class.count(a)).to eq 0
+        end
+      end
+
+      context 'given an array of one number' do
+        let(:a) { [ rand(-10..10) ] }
+
+        it 'returns 0' do
+          expect(described_class.count(a)).to eq 0
+        end
+      end
+
+      context 'given an array of two numbers that sum to 0' do
+        let(:a) { [-3, 3] }
+
+        it 'returns 0' do
+          expect(described_class.count(a)).to eq 0
+        end
+      end
+
+      context 'given a triplet that does not sum to 0' do
+        let(:a) { [1, -1, 0] }
+        it 'returns 1' do
+          expect(described_class.count(a)).to eq 1
+        end
+      end
+
+      context 'given [1, 2, 3]' do
+        let(:a) { [1, 2, 3] }
+
+        it 'returns 0' do
+          expect(described_class.count(a)).to eq 0
+        end
+      end
+
+      context 'given an array with 2 possible triples' do
+        let(:a) { [-1, -1, 0, 1] }
+
+        it 'returns 2' do
+          expect(described_class.count(a)).to eq 2
+        end
+      end
+    end
+  end
+end
