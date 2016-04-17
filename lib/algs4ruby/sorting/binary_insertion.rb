9e7b57e6c2fcd979fc2883ffa4eb3105df0cb393
Remco Peereboom <rpeere@yandex.com>
2016-04-17 15:04:25 +0200

    Implemented BinaryInsertion (BinaryInsertion)
    The original implementation was somewhat limited compared to the other
    sorting algorithms. I modified it to make it work with different
    comparators and without them, as well as being able to sort arbitrary
    slices of arrays/strings and to implement both mutating and non-mutating
    versions.
    
    Added the necessary methods.
    
    Added tests.
    
    Added documentation for all methods.
    
    Added testing client.
    
    Added license information.

--- lib/algs4ruby/sorting/binary_insertion.rb
+++ lib/algs4ruby/sorting/binary_insertion.rb
@@ -1,12 +1,22 @@
 module Algs4Ruby
-  # The Insertion module provides methods to insertion sort an array (or other
-  # indexable collection). It requires that all elements in the collection
-  # have a total order.
+  # BinaryInsertion
   #
-  # This implementation makes ~ 1/2 * n**2 compares and exchanges in the worst
-  # case, so it is not suitable for sorting large arbitrary arrays. More
-  # precisely, the number of exchanges is exactly equal to the number of
-  # inversions. So, for example, it sorts partially-sorted array in linear time.
+  # The {BinaryInsertion} module provides methods to insertion sort an array
+  # (or other indexable collections). It requires that all elements have a
+  # total order.
+  #
+  # This implementation makes ~ n log n compares for any array, however in the
+  # worst case the running time is quadratic because the number of array
+  # accesses can be proportional to n**2 (e.g. if the array is reverse sorted).
+  # As such, it is not suitable for sorting large arrays (unless the number of
+  # inversions is known to be small).
+  #
+  # Binary insertion sorting works because we the sorting algorithm maintains
+  # a sorted subarray into which unsorted elements are inserted. Since the
+  # sub-array is sorted, it can use binary search to quickly find the place of
+  # insertion, reducing the number of comparissons from O(n) to O(log n). This
+  # is useful if (and really only if) the cost of comparisson is significant
+  # (e.g. when sorting large strings or objects with many attributes).
   #
   # The sorting algorithm is stable and uses O(1) extra memory for the mutating
   # sort and O(n) for the non-mutating sort.
@@ -16,16 +26,16 @@ module Algs4Ruby
   # http://algs4.cs.princeton.edu
   #
   # Testing client:
-  #   ruby insertion.rb in1 in2 in3
-  #   ruby insertion.rb < in
-  #   ruby insertion.rb
+  #   ruby binary_insertion.rb in1 in2 in3
+  #   ruby binary_insertion.rb < in
+  #   ruby binary_insertion.rb
   #     string1
   #     string2
   #     string3
   #
   # Read lines from ARGF and insertion sorts them.
   # Then prints them to standard output in ascending order.
-  module Insertion
+  module BinaryInsertion
     class << self
       # Sort the given array, without mutating the source array.
       # @overload sort(array)
@@ -53,27 +63,29 @@ module Algs4Ruby
       #   @return [Array] the source array in sorted order.
       def sort!(array, slice = 0...array.size, &block)
         slice.each do |i|
-          while i > slice.first && less?(array, i, i - 1, &block)
-            swap(array, i, i - 1)
-            i -= 1
+          value = array[i]
+
+          low = slice.min
+          high = i
+
+          while low < high
+            middle = low + (high - low) / 2
+            if less?(array, i, middle, &block)
+              high = middle
+            else
+              low = middle + 1
+            end
           end
+
+          i.downto(low) { |j| array[j] = array[j - 1] }
+          array[low] = value
         end
+
 
         array
       end
 
       private
-
-      # Swap to elements of the array in place.
-      # @param array [Array] the array to mutate.
-      # @param a [Integer] an index into the array (unchecked).
-      # @param b [Integer] an index into the array (unchecked).
-      # @return [void]
-      def swap(array, a, b)
-        temp = array[a]
-        array[a] = array[b]
-        array[b] = temp
-      end
 
       # @overload less?(array, a, b)
       #   Is the element at index a less than the element at index b according
@@ -105,19 +117,20 @@ module Algs4Ruby
 
   # Testing client
   #
-  # Read lines from ARGF and insertion sorts them.
+  # Read lines from ARGF and binary insertion sorts them.
   # Then prints them to standard output in ascending order.
   if __FILE__ == $PROGRAM_NAME
     lines = ARGF.readlines
-    sorted_lines = Insertion.sort(lines)
+    sorted_lines = BinaryInsertion.sort(lines)
 
     sorted_lines.each { |l| puts l }
   end
 end
 
 ###############################################################################
-# The code in this file is based on the java code in Insertion from alg4.jar
-# library. That library can be found at http://algs4.cs.princeton.edu
+# The code in this file is based on the java code in BinaryInsertion
+# from alg4.jar library. That library can be found at
+# http://algs4.cs.princeton.edu
 # You can find more information on the alg4.jar library on that website.
 #
 #
@@ -125,15 +138,15 @@ end
 # Kevin Wayne. Attribution of the algorithms can be found at the princeton
 # website as well.
 #
-# insertion.rb is free software: you can redistribute it and/or modify
+# binary_insertion.rb is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# insertion.rb is distributed in the hope that it will be useful,
+# binary_insertion.rb is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 # You should have received a copy of the GNU General Public License
-# along with insertion.rb. If not, see http://www.gnu.org/licenses.
+# along with binary_insertion.rb. If not, see http://www.gnu.org/licenses.
 ################################################################################
---
+++ spec/algs4ruby/sorting/binary_insertion_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+module Algs4Ruby
+  describe BinaryInsertion do
+    it_behaves_like 'a sorting algorithm'
+  end
+end
