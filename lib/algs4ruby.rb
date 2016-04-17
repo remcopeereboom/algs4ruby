fe2c112762d78271328716a777470340f0e3f320
Remco Peereboom <rpeere@yandex.com>
2016-04-17 15:04:25 +0200

    Implemented BinaryInsertion (BinaryInsertion)
    The original implementation was somewhat limited compared to the other
    sorting algorithms. I modified it to make it work with different
    comparators and without them, as well as being able to sort arbitrary
    slices of arrays/strings and to implement both mutating and non-mutating
    versions.
    
    Added the necessary methods.
    
    Added tests.
    
    Added documentation for all methods.
    
    Added testing client.
    
    Added license information.

--- lib/algs4ruby.rb
+++ lib/algs4ruby.rb
@@ -29,6 +29,7 @@ require 'algs4ruby/fundamentals/union_find_quick_union'
 require 'algs4ruby/fundamentals/union_find_weighted_quick_union'
 
 # Sorting #
+require 'algs4ruby/sorting/binary_insertion'
 require 'algs4ruby/sorting/insertion'
 require 'algs4ruby/sorting/min_pq'
 require 'algs4ruby/sorting/max_pq'
